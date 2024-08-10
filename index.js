02c5c0e7cb0c0d825602bb6567f903a73c79b9eb
Palady56 <paladyultra@gmail.com>
2024-08-10 23:27:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// require('dotenv').config();
+
+// const apiKey = process.env.OMDB_API_KEY;
+// const baseUrl = process.env.BASE_URL
+const apiKey = "de537de8"
+const baseUrl = "http://www.omdbapi.com/"
+
+const searchInput = document.getElementById('searchInput');
+const resultsContainer = document.getElementById('results');
+
+async function searchMovies(query) {
+  const response = await fetch(`${baseUrl}?apikey=${apiKey}&s=${query}`);
+  const data = await response.json();
+  return data.Search; 
+}
+
+function displayResults(movies) {
+  resultsContainer.innerHTML = ''; 
+  if (movies) {
+    movies.forEach(movie => {
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie');
+      movieElement.innerHTML = `
+                <img src="${movie.Poster !== "N/A" ? movie.Poster : 'placeholder.jpg'}" alt="Poster">
+                <h3>${movie.Title} (${movie.Year})</h3>
+            `;
+      resultsContainer.appendChild(movieElement);
+    });
+  } else {
+    resultsContainer.innerHTML = '<p>No results found</p>';
+  }
+}
+
+function displayResults(movies) {
+  resultsContainer.innerHTML = '';
+  if (movies) {
+      movies.forEach(movie => {
+          const movieElement = document.createElement('div');
+          movieElement.classList.add('movie');
+
+          // Заглушка
+          const poster = movie.Poster !== "N/A" ? movie.Poster : './placeholder.png';
+          movieElement.innerHTML = `
+              <img src="${poster}" alt="Poster">
+              <h3>${movie.Title} (${movie.Year})</h3>
+          `;
+          resultsContainer.appendChild(movieElement);
+      });
+  } else { 
+      resultsContainer.innerHTML = '<p>No results found</p>';
+  }
+}
+
+// func to prevent frequent requests
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
+
+searchInput.addEventListener('input', debounce(async function () {
+  const query = searchInput.value.trim();
+  if (query.length > 2) {
+    const movies = await searchMovies(query);
+    displayResults(movies);
+  } else {
+    resultsContainer.innerHTML = ''; 
+  }
+}, 300));
