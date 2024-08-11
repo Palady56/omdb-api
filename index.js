fc67cdf694b18a4fabf1acc5c54e6cd43f98e9d4
Palady56 <paladyultra@gmail.com>
2024-08-11 14:48:57 +0300

    add error handling (fetch request)

--- index.js
+++ index.js
@@ -7,16 +7,32 @@ const baseUrl = "http://www.omdbapi.com/"
 
 const searchInput = document.getElementById('searchInput');
 const resultsContainer = document.getElementById('results');
-const filmCountElement = document.getElementById('filmCount'); 
+const filmCountElement = document.getElementById('filmCount');
 
 async function searchMovies(query) {
-  const response = await fetch(`${baseUrl}?apikey=${apiKey}&s=${query}`);
-  const data = await response.json();
-  return data.Search; 
+  try {
+    const response = await fetch(`${baseUrl}?apikey=${apiKey}&s=${query}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.Response === "False") {
+      throw new Error(data.Error);
+    }
+
+    return data.Search;
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    displayError(error.message);
+    return null;
+  }
 }
 
 function displayResults(movies) {
-  resultsContainer.innerHTML = ''; 
+  resultsContainer.innerHTML = '';
   if (movies) {
     movies.forEach(movie => {
       const movieElement = document.createElement('div');
@@ -35,22 +51,22 @@ function displayResults(movies) {
 function displayResults(movies) {
   resultsContainer.innerHTML = '';
   if (movies) {
-      movies.forEach(movie => {
-          const movieElement = document.createElement('div');
-          movieElement.classList.add('movie');
+    movies.forEach(movie => {
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie');
 
-          // Заглушка
-          const poster = movie.Poster !== "N/A" ? movie.Poster : './placeholder.png';
-          movieElement.innerHTML = `
+      // Заглушка
+      const poster = movie.Poster !== "N/A" ? movie.Poster : './placeholder.png';
+      movieElement.innerHTML = `
               <img src="${poster}" alt="Poster">
               <h3>${movie.Title} (${movie.Year})</h3>
           `;
-          resultsContainer.appendChild(movieElement);
-          filmCounter(movies.length);
-      });
-  } else { 
-      resultsContainer.innerHTML = '<p>No results found</p>';
-      filmCounter(0);
+      resultsContainer.appendChild(movieElement);
+      filmCounter(movies.length);
+    });
+  } else {
+    resultsContainer.innerHTML = '<p>No results found</p>';
+    filmCounter(0);
   }
 }
 
@@ -73,7 +89,7 @@ searchInput.addEventListener('input', debounce(async function () {
     const movies = await searchMovies(query);
     displayResults(movies);
   } else {
-    resultsContainer.innerHTML = ''; 
+    resultsContainer.innerHTML = '';
     filmCounter(0)
   }
 }, 300));
